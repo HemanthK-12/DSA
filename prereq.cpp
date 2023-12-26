7bc3c822d34f8676c88e0816fa305f1d56f4b486
HemanthK-12 <hemanthkarthikeya12@gmail.com>
2023-12-27 01:10:19 +0530

    Added Prerequistes File

---
+++ prereq.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc.h>
+using namespace std;
+
+int main()
+{
+    freopen("input.txt", "r", stdin);//Reads input from input.txt,with stdin as input stream
+    freopen("output.txt", "w", stdout);//Writes output to output.txt,with stdout as output stream
+
+
+    return 0;
+}
