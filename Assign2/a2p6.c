c849872d04f7ca679da874c14bdbcd13e5963519
HemanthK-12 <hemanthkarthikeya12@gmail.com>
2024-01-31 01:12:01 +0530

    Add arraysum function and implement main logic for calculating sum of subarrays

--- Assign2/a2p6.c
+++ Assign2/a2p6.c
@@ -1,5 +1,26 @@
 #include<stdio.h>
-int main()
-{
-    return 0;
+int arraysum(int arr[],int p,int q){
+    int sum=0;
+    for(int i=p;i<=q;i++){
+        sum=sum+arr[i];
+    }
+    return sum;
+}
+
+int main(){
+    int n,q;
+    
+    scanf("%d %d",&n,&q);
+    int arr[n];
+    for(int i=1;i<=n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int arr2[q],arr3[q];
+    for(int i=0;i<q;i++){
+        scanf("%d %d",&arr2[i],&arr3[i]);
+    }
+    for(int i=0;i<q;i++){
+        printf("%d\n",arraysum(arr,arr2[i],arr3[i]));
+    }
+
 }
