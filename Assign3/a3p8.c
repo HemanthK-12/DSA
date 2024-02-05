261626897e06c1c0349e0a8b96524a86f5c13f6a
HemanthK-12 <hemanthkarthikeya12@gmail.com>
2024-02-05 20:55:27 +0530

    Fix loop iteration and add output formatting in a3p8.c

--- Assign3/a3p8.c
+++ Assign3/a3p8.c
@@ -7,13 +7,16 @@ int main()
   llint arr[n];
   for(int i=0;i<n;i++)
     scanf("%lld",&arr[i]);
+
   for(int i=0;i<n;i++)
   {
-    for(llint j=0;j<=59;j++)
+    for(llint j=59;j>=0;j--)
     {
     llint a=(arr[i])&((llint)1<<j);
     a=(a==0)?0:1;
-    
+    printf("%lld",a);
+    }
+    printf("\n");
   }
   return 0;
 }
