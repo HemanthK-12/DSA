7983421f07daf36015ae0fcadf07798d569497e2
HemanthK-12 <hemanthkarthikeya12@gmail.com>
2024-02-05 12:29:13 +0530

    Fix palindrome substring length calculation

--- Assign3/a3p1.c
+++ Assign3/a3p1.c
@@ -1,41 +1,56 @@
+/*
+Input
+The first line contains an integer n (1 ≤ n ≤ 3 ∗ 103).
+The second line contains a string of length n consisting of only lowercase English alphabet.
+Your solution should run in O(n2) time complexity
+Output
+The length of the longest substring which is also a palindrome.
+*/
 #include <stdio.h>
 
-int isPalindrome(char* str, int start, int end)
+int longestPalSubstr(char *str,int n)
 {
-    while (start < end)
-    {
-        if (str[start] != str[end])
-            return 0;
-        start++;
-        end--;
-    }
-    return 1;
-}
+    int maxLength = 1, start = 0;
+    int table[n][n];
 
-int longestPalindrome(char* str,int n) {
-    int maxLength = 0;
+    for (int i = 0; i < n; ++i)
+        for(int j=0;j<n;j++)
+            table[i][j] =(i==j)?1:0;
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n - 1; ++i)
     {
-        for (int j = i; j < n; j++)
+        if (str[i] == str[i + 1])
         {
-            if (isPalindrome(str, i, j))
-            {
-                int length = j - i + 1;
-                if (length > maxLength)
-                    maxLength = length;
-            }
+            table[i][i + 1] = 1;
+            start = i;
+            maxLength = 2;
         }
     }
 
+    for (int k = 3; k <= n; ++k)
+    {
+        for (int i = 0; i < n - k + 1; ++i)
+        {
+            int j = i + k - 1;
+            if (table[i + 1][j - 1] && str[i] == str[j])
+            {
+                table[i][j] = 1;
+                if (k > maxLength)
+                {
+                    start = i;
+                    maxLength = k;
+                }
+            }
+        }
+    }
     return maxLength;
 }
 
 int main() {
     int n;
-    scanf("%d",&n);
-    char arr[n];
-    scanf("%s",arr);
-    printf("%d\n", longestPalindrome(arr,n));
+    scanf("%d", &n);
+    char str[n];
+    scanf("%s", str);
+    printf("%d\n", longestPalSubstr(str,n));
     return 0;
 }
