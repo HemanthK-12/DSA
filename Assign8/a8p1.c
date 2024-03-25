891a4dea85468e5f19474db0184db3de15ad0283
HemanthK-12 <hemanthkarthikeya12@gmail.com>
2024-03-25 17:01:42 +0530

    Shortened code

--- Assign8/a8p1.c
+++ Assign8/a8p1.c
@@ -1,97 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-// Stack structure definition
-struct Stack {
+struct Stack
+{
     int top;
     unsigned capacity;
-    char* array;
+    char *array;
 };
-
-// Stack Operations
-struct Stack* createStack(unsigned capacity) {
-    struct Stack* stack = (struct Stack*) malloc(sizeof(struct Stack));
-    if (!stack) 
+struct Stack *createStack(unsigned capacity)
+{
+    struct Stack *stack = (struct Stack *)malloc(sizeof(struct Stack));
+    if (!stack)
         return NULL;
     stack->top = -1;
     stack->capacity = capacity;
-    stack->array = (char*) malloc(stack->capacity * sizeof(char));
+    stack->array = (char *)malloc(stack->capacity * sizeof(char));
     return stack;
 }
-
-int isFull(struct Stack* stack) {   
-    return stack->top == stack->capacity - 1;
-}
-
-int isEmpty(struct Stack* stack) {
+int isEmpty(struct Stack *stack)
+{
     return stack->top == -1;
 }
-
-void push(struct Stack* stack, char item) {
-    if (isFull(stack))
+void push(struct Stack *stack, int item)
+{
+    if (stack->top == stack->capacity - 1)
         return;
     stack->array[++stack->top] = item;
 }
-
-char pop(struct Stack* stack) {
-    if (isEmpty(stack))
-        return -1;          
-    return stack->array[stack->top--];
+int pop(struct Stack *stack)
+{
+   return (isEmpty(stack))?-1:stack->array[stack->top--];
 }
-
-// Function to check if character x is an opening bracket and character y is an matching closing bracket
-int isMatchingPair(char character1, char character2) {
-   if (character1 == '(' && character2 == ')')
-     return 1;
-   else if (character1 == '{' && character2 == '}')
-     return 1;
-   else if (character1 == '[' && character2 == ']')
-     return 1;
-   else
-     return 0;
+int isMatchingPair(char character1, char character2)
+{
+    return ((character1 == '(' && character2 == ')')||(character1 == '{' && character2 == '}')||(character1 == '[' && character2 == ']'))?1:0;
 }
-
-// Function to check if brackets in given expression are balanced
-int areBracketsBalanced(char exp[]) {
+int areBracketsBalanced(char exp[])
+{
     int i = 0;
-  
-    // Create a stack of capacity equal to expression size
-    struct Stack* stack = createStack(strlen(exp));
-    if(!stack) 
+    struct Stack *stack = createStack(strlen(exp));
+    if (!stack)
         return -1;
-
-    while (exp[i]) {
-        // If the exp[i] is a starting bracket then push it
+    while (exp[i])
+    {
         if (exp[i] == '{' || exp[i] == '(' || exp[i] == '[')
             push(stack, exp[i]);
-
-        // If exp[i] is an ending bracket then pop from stack and check if the popped bracket is a matching pair
-        if (exp[i] == '}' || exp[i] == ')' || exp[i] == ']') {
-            // If we see an ending bracket without a pair then return false
-            if (isEmpty(stack)) {
-              return 0;
-            } else if ( !isMatchingPair(pop(stack), exp[i]) ) {
-              return 0;
-            }
-        }
+        if (exp[i] == '}' || exp[i] == ')' || exp[i] == ']')
+            if ((isEmpty(stack))||(!isMatchingPair(pop(stack), exp[i])))
+                return 0;
         i++;
     }
-
-    // If there is something left in expression then there is a starting bracket without a closing bracket
-    if (isEmpty(stack)) {
-        return 1; // balanced
-    } else {
-        return 0;  // not balanced
-    }
+    return (isEmpty(stack))?1:0;
 }
-
-// Driver program to test above function
-int main() {
+int main()
+{
     char exp[10000];
     printf("Enter the expression: ");
     scanf("%[^\n]", exp);
-    getchar();  // consume newline character
+    getchar(); // consume newline character
     if (areBracketsBalanced(exp))
         printf("Balanced \n");
     else
